a7890dc442aeb35d0814241ade552f4b1a8d4427
Ian <strifter.if@gmail.com>
2022-11-11 16:27:56 +0100

    Actualizado Fase 1 acabada

--- simulator.c
+++ simulator.c
@@ -10,11 +10,42 @@ pthread_cond_t cond1;
 pthread_cond_t cond2;
 int done;
 
-int jobQueue;
-int readyQueue;
-int waitQueue;
+int num_process = 0;
+int max_process = 200;
 
- void* cpu_clock(void *arg) {
+PCB * pcb_queue;
+pthread_t pth[12];
+
+PCB generate_process() {
+    if (num_process < 200) {
+        PCB pcb;
+        pcb.tid = num_process;
+        pcb.status = 0;
+        pcb.quantum = 
+        pcb_queue[max_process] = pcb;
+        num_process++;
+        max_process--;
+        printf("\nSe ha generado el proceso con id %d\n", pcb.tid);
+        fflush(stdout);
+    }
+ }
+
+ void * process() {
+    // nanosleep()
+    int tid = gettid();
+    printf("Executing process %d", gettid());
+    if (pthread_join(th[i], NULL) != 0) perror("Failed to join thread\n");
+ }
+
+ void execute_process(void * arg) {
+    if (num_processors > 0) {
+        if (pthread_create(&pth[num_processors], NULL, &process, &) != 0) perror("Failed to create thread\n");
+        num_processors--;
+
+    }
+ }
+
+ void * cpu_clock(void * arg) {
     printf("Im the clock\n");
     while (1) {
         pthread_mutex_lock(&mutex);
@@ -27,18 +58,42 @@ int waitQueue;
     }
  }
 
- void* timer(void *arg) {
+ void * timer1(void * arg) {
     pthread_mutex_lock(&mutex);
-    printf("Im the timer\n");
+    printf("Im the timer 1\n");
+    // count = 20000; 
+    args_t *args = arg;
+    args->pcb.tid = gettid();
+    printf("I'm tid %d\n", args->pcb.tid);
+    while (1) {
+        done++;
+        args->cycles1++;
+        if (args->cycles1 == args->count2) {
+            //printf("\n%d ha contado %d ciclos\n", gettid(), args->cycles1);
+            //fflush(stdout);
+            args->cycles1 = 0;
+            execute_process();
+        }
+        pthread_cond_signal(&cond1);
+        pthread_cond_wait(&cond2, &mutex);
+    }
+ }
+
+  void * timer2(void * arg) {
+    pthread_mutex_lock(&mutex);
+    printf("I'm the timer 2\n");
+    // count = 20000; 
     args_t *args = arg;
     args->pcb.tid = gettid();
     printf("Im tid %d\n", args->pcb.tid);
     while (1) {
         done++;
-        args->cycles++;
-        if (args->cycles == args->quantum) {
-            printf("\nSe han contado %d ciclos\n", args->cycles);
-            args->cycles = 0;
+        args->cycles2++;
+        if (args->cycles2 == args->count1) {
+            // printf("\n%d ha contado %d ciclos\n", gettid(), args->cycles2);
+            // fflush(stdout);
+            args->cycles2 = 0;
+            generate_process();
         }
         pthread_cond_signal(&cond1);
         pthread_cond_wait(&cond2, &mutex);
@@ -52,25 +107,26 @@ int main(int argc, char * argv[]) {
 
     pthread_t th[NUM_THREADS];
     args_t args;
-    args.cycles = 0;
-    args.quantum = 20000;
+    args.cycles1 = 0;
+    args.cycles2 = 0;
+    args.count1 = 10000;
+    args.count2 = 50000;
 
-    //struct PCB pcb;
+    pcb_queue = (PCB*) malloc(200 * sizeof(PCB));
 
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&cond1, NULL);
     pthread_cond_init(&cond2, NULL);
-    if (pthread_create(&th[0], NULL, cpu_clock, NULL) != 0) perror("Failed to create th1\n");
-    if (pthread_create(&th[1], NULL, timer, &args) != 0) perror("Failed to create th2\n");
-    if (pthread_create(&th[2], NULL, &timer, &args) != 0) perror("Failed to create th2\n");
-    
+    if (pthread_create(&th[0], NULL, &cpu_clock, NULL) != 0) perror("Failed to create th1\n");
+    if (pthread_create(&th[1], NULL, &timer1, &args) != 0) perror("Failed to create th2\n");
+    if (pthread_create(&th[2], NULL, &timer2, &args) != 0) perror("Failed to create th3\n");
+
     for (int i = 0; i < NUM_THREADS; i++) {
-        if (pthread_join(th[i], NULL) != 0) perror("Failed to join th1\n");
+        if (pthread_join(th[i], NULL) != 0) perror("Failed to join thread\n");
     }
 
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&cond1);
     pthread_cond_destroy(&cond2);
     return 0;
-
 }
--- structs.h
+++ structs.h
@@ -7,28 +7,36 @@
 pid_t gettid(void);
 void *malloc(size_t size);
 
-struct PCB {
-    pid_t tid;
-};
+typedef struct {
+    pid_t tid; // process id
+    int status; // 0 = ready; 1 = waitting; 2 = blocked;
+    double quantum; // process 
+} PCB;
 
-struct cfs_rq {
-
-};
-
-struct rt_rq {
-
-};
-
-struct run_queue {
-    unsigned int np_running;
-    struct cfs_rq cfs;
-    struct rt_rq rt;
-    struct PCB *curr;
-    int cpu;
+struct scheduler {
+    struct PCB * runqueue;
 };
 
 typedef struct {
-    int cycles;
-    int quantum;
-    struct PCB pcb;
+    int cycles1;
+    int cycles2;
+    int count1;
+    int count2;
+    PCB pcb;
 } args_t;
+
+// struct cfs_rq {
+
+// };
+
+// struct rt_rq {
+
+// };
+
+// struct run_queue {
+//     unsigned int np_running;
+//     struct cfs_rq cfs;
+//     struct rt_rq rt;
+//     struct PCB *curr;
+//     int cpu;
+// };
